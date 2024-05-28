305445a3c0ab7f7fa83ed02e2e67b183fc1684e3
吴晨 <2467337566@qq.com>
2024-05-29 06:41:15 +0800

    feat:添加分包和css配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div>{{ count }}</div>
   <button @click="count++">+</button>
+  <div>{{ moment().format('YYYY-MM-DD HH:mm:ss') }}</div>
 </template>
 
 <script setup lang="ts">
+import moment from 'moment'
 import { ref } from 'vue'
 const count = ref(0)
 </script>
+
+<style lang="less">
+@aqua: aqua;
+html,
+body {
+  background-color: @aqua;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const { Configuration } = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 /**
  * @type {Configuration}
@@ -11,13 +12,16 @@ const config = {
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[chunkhash].js',
+    clean: true
   },
+  stats: 'errors-only',
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin()
   ],
   module: {
     // loader是用来处理对应文件的 要解析什么文件就装对应类型的loader
@@ -33,8 +37,34 @@ const config = {
       {
         test: /\.vue$/,
         use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        // 所有的样式都是由 css-loader 统一去处理的
+        // style-loader 只是把 css-loader 处理后的内容插入到 html 文件中
+        use: [MiniCssExtractPlugin.loader, 'css-loader'] // 顺序不能变 从右向左
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
       }
     ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        moment: {
+          name: 'moment',
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/]moment/
+        },
+        common: {
+          name: 'common',
+          chunks: 'all',
+          minChunks: 2 // 使用次数大于2次就会被分割出来
+        }
+      }
+    }
   }
 }
 
